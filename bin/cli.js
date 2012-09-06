68a404076e699179485dbf881d548d2c9fa62923
Michael Nisi <michael.nisi@gmail.com>
2012-09-06 17:35:25 +0100

    Write push module

--- bin/cli.js
+++ bin/cli.js
@@ -1,20 +1,13 @@
 #!/usr/bin/env node
 
-var pushup = require('../index.js')
-  , statSync = require('fs').statSync
-  , getProps = require('../lib/getProps.js')
-  , show = require('../lib/show.js')
+var getProps = require('../lib/getProps.js')
   , statIsGit = require('../lib/statIsGit.js')
   , push = require('../lib/push.js')
-  , cp = require('../lib/cp.js')
-  , cpr = require('../lib/cpr.js')
 
 ;(function () {
   var arg = process.argv.splice(2)
     , props = getProps()
     , path = arg[0]
-
-  
   
   if (statIsGit(path)) { 
     push(props, path)
@@ -22,6 +15,3 @@ var pushup = require('../index.js')
     console.log('Not implemented yet')
   }
 })()
-
-function isGit (path) {
-}
