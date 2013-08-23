aa07b0e33dfe973bbfc9aadde033371e8129e065
Michael Nisi <michael.nisi@gmail.com>
2013-08-23 20:37:26 +0200

    Extract modules

--- bin/cli.js
+++ bin/cli.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-var getProps = require('../lib/getProps.js')
+var opts = require('../lib/opts.js')
   , push = require('../lib/push.js')
-  , statIsGit = require('../lib/statIsGit.js')
   , cp = require('../lib/cp.js')
   , cpr = require('../lib/cpr.js')
+  , isgit = require('isgit')
   , statSync = require('fs').statSync
 
 ;(function () {
   var arg = process.argv.splice(2)
-    , props = getProps()
+    , opts = opts()
     , path = arg[0] || process.cwd()
 
-  if (statIsGit(path)) {
-    push(props, path)
+  if (isgit(path)) {
+    push(opts, path)
   } else if (statSync(path).isFile()) {
-    cp(props, arg)
+    cp(opts, arg)
   } else {
-    cpr(props, path)
+    cpr(opts, path)
   }
 })()
--- index.js
+++ index.js
@@ -1,98 +1,27 @@
-module.exports = pushup
 
-var validateProps = require('./lib/validateProps.js')
-  , es = require('event-stream')
-  , knox = require('knox')
-  , Stream = require('stream').Stream
-  , join = require('path').join
+// pushup - copy files to S3
 
-function pushup (props, callback) {
-  var stream = new Stream()
-    , client = knox.createClient(props)
-    , error = validateProps(props)
-    , ended = false
-    , paused = false
-    , files = null
-    , count = 0
+var knox = require('knox')
+  , Transform = require('stream').Transform
+  , opts = require('./lib/opts.js')
 
-  if (error) {
-    if (callback) callback(error)
-    return error
-  }
+module.exports = function (opts) {
+  opts = opts || opts()
 
-  stream.readable = true
-  stream.writable = true
+  var stream = new Transform()
+    , client = knox.createClient(opts)
 
-  stream.resume = function () {
-    if (ended) {
-      throw new Error('Resume after ended')
-    }
-    paused = false
-    upload()
-    stream.emit('resume')
-  }
-
-  stream.pause = function () {
-    if (ended) {
-      throw new Error('Pause after ended')
-    }
-    paused = true
-    stream.emit('pause')
-  }
-
-  stream.end = function () {
-    if (count) return
-    ended = true
-    stream.emit('end')
-    if (callback) callback(error)
-  }
-
-  stream.write = function (data) {
-    if (!files) {
-      count = 0
-      files = []
-    }
-    
-    files.push(data)
-     
-    upload()
-
-    return true 
-  }
-
-  function upload () {
-    if (!files.length && !count) {
-      stream.end()
-      return
-    }
-   
-    stream.pause()
-
-    var file = files.shift()
-    
-    if (!file) {
-      return
-    }
-
-    var entry = client.putFile(file, '/' + file, responseHandler)
-    
-    count++
-
-    function responseHandler (err, res) {
-      count--
-      if (err) {
-        error = err
-        stream.emit('error', err)
-        if (!ended) stream.resume()
-        return
+  stream._transform = function (chunk, enc, cb) {
+    var file = chunk.toString()
+    var entry = client.putFile(file, '/' + file, function (er, res) {
+      if (er) {
+        stream.emit('error', er)
       }
-      stream.emit('data', res.socket._httpMessage.url + '\n')
-      if (!ended) stream.resume()
-    }
-
-    entry.name = file
-    
-    stream.emit('entry', entry)
+      stream.push(res.socket._httpMessage.url)
+      entry.name = file
+      stream.emit('entry', entry)
+      cb()
+    })
   }
 
   return stream
